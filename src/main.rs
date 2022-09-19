93709cb4c64367a00c62cf04304134f7c44d6630
aix <651360107@qq.com>
2022-09-19 17:16:16 +0800

    list

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,14 @@ fn run() {
         .version(crate_version!())
         .subcommand(cmd::conn::subcommand())
         .subcommand(cmd::show::subcommand())
+        .subcommand(cmd::list::subcommand())
         .subcommand(cmd::update::subcommand())
         .get_matches();
     match matches.subcommand() {
         //cmd
         ("conn", Some(ucpe)) => cmd::conn::run(ucpe),
         ("show", Some(ucpe)) => cmd::show::run(ucpe),
+        ("list", Some(ucpe)) => cmd::list::run(ucpe),
         ("update", Some(ucpe)) => cmd::update::run(ucpe),
         _ => eprintln!("No subcommand chosen. use --help | -h to view the subcommands."),
     }
